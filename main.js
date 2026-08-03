c44fe5539c948fe83749e6245237aad66e098f66
pedropinell <superpedritopinell@gmail.com>
2026-08-02 21:52:37 -0700

    a

--- main.js
+++ main.js
@@ -34,32 +34,7 @@ container.innerHTML = records.map(r => `
 }
 renderEpisodes(topEpisodes);
 
-import { createAnimatable, utils } from 'animejs';
 
-const $demos = document.querySelector('#docs-demos');
-const $demo = document.querySelector('.docs-demo.is-active');
-
-let bounds = $demo.getBoundingClientRect();
-const refreshBounds = () => bounds = $demo.getBoundingClientRect();
-
-const animatableSquare = createAnimatable('.square', {
-  x: 500, // Define the x duration to be 500ms
-  y: 500, // Define the y duration to be 500ms
-  ease: 'out(3)',
-});
-
-const onMouseMove = e => {
-  const { width, height, left, top } = bounds;
-  const hw = width / 2;
-  const hh = height / 2;
-  const x = utils.clamp(e.clientX - left - hw, -hw, hw);
-  const y = utils.clamp(e.clientY - top - hh, -hh, hh);
-  animatableSquare.x(x); // Animate the x value in 500ms
-  animatableSquare.y(y); // Animate the y value in 500ms
-}
-
-window.addEventListener('mousemove', onMouseMove);
-$demos.addEventListener('scroll', refreshBounds);
 
 
 
